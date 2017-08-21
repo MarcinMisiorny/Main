2e917ecb4a2f18de77be9d6c528b9c4ec77384b6
MarcinMisiorny <30782851+MarcinMisiorny@users.noreply.github.com>
2017-08-21 20:27:48 +0200

    Update 06DDL_zadania.sql

--- Systemy Baz Danych/1. SQL/06DDL_zadania.sql
+++ Systemy Baz Danych/1. SQL/06DDL_zadania.sql
@@ -1,107 +1,109 @@
 /*
 * --------------------------------------------
-* Rozdzia³ 6. Jêzyk definiowania danych DDL –
+* RozdziaÅ‚ 6. JÄ™zyk definiowania danych DDL â€“
 * zadania
 * --------------------------------------------
 * 
 * Plik z zadaniami: 06DDL_zadania.pdf
 * 
-* Plik tworz¹cy bazê do æwiczeñ: Pldemobld.sql
+* Plik tworzÄ…cy bazÄ™ do Ä‡wiczeÅ„: Pldemobld.sql
 * 
 */
 
 --------------------------------------------------------
--- 1. Utwórz poni¿sze relacje.
+-- 1. UtwÃ³rz poniÅ¼sze relacje.
 
 -- 
 -- Nazwa relacji: PROJEKTY
--- Nazwa				Typ									Rozmiar		W³asnoœci
--- ID_PROJEKTU			Liczba ca³kowita					4			Klucz podstawowy o nazwie PROJEKTY_PK
--- OPIS_PROJEKTU		£añcuch znaków zmiennej d³ugoœci	20			Wymagany, klucz unikalny o nazwie PROJEKTY_UK
--- DATA_ROZPOCZECIA		Data											Domyœlnie data systemowa
--- DATA_ZAKONCZENIA		Data											PóŸniejsza ni¿ DATA_ROZPOCZECIA, ograniczenie ma nosiæ nazwê PROJEKTY_DATY_CHK
--- FUNDUSZ				Liczba								7,2			Wiêkszy lub równy 0, ograniczenie ma nosiæ nazwê PROJEKTY_FUNDUSZ_CHK
+-- Nazwa		Typ					Rozmiar		WÅ‚asnoÅ›ci
+-- ID_PROJEKTU		Liczba caÅ‚kowita			4		Klucz podstawowy o nazwie PROJEKTY_PK
+-- OPIS_PROJEKTU	ÅaÅ„cuch znakÃ³w zmiennej dÅ‚ugoÅ›ci	20		Wymagany, klucz unikalny o nazwie PROJEKTY_UK
+-- DATA_ROZPOCZECIA	Data							DomyÅ›lnie data systemowa
+-- DATA_ZAKONCZENIA	Data							PÃ³Åºniejsza niÅ¼ DATA_ROZPOCZECIA, ograniczenie ma nosiÄ‡ nazwÄ™ PROJEKTY_DATY_CHK
+-- FUNDUSZ		Liczba					7,2		WiÄ™kszy lub rÃ³wny 0, ograniczenie ma nosiÄ‡ nazwÄ™ PROJEKTY_FUNDUSZ_CHK
 -- 
 -- 
 -- Nazwa relacji: PRZYDZIALY
--- Nazwa				Typ									Rozmiar		W³asnoœci
--- ID_PROJEKTU			Liczba ca³kowita					4			Niepusty klucz obcy o nazwie PRZYDZIALY_FK_01 do kolumny ID_PROJEKTU w relacji PROJEKTY
--- NR_PRACOWNIKA		Liczba ca³kowita					6			Niepusty klucz obcy o nazwie PRZYDZIALY_FK_02 do kolumny ID_PRAC w relacji PRACOWNICY
--- OD					Data											Domyœlnie data systemowa
--- DO					Data											PóŸniejsza ni¿ OD, ograniczenie ma nosiæ nazwê PRZYDZIALY_DATY_CHK
--- STAWKA				Liczba								7,2			Wiêksza od 0, ograniczenie ma nosiæ nazwê PRZYDZIALY_STAWKA_CHK
--- ROLA					£añcuch znaków o zmiennej d³ugoœci	20			Dopuszczalne wartoœci to: ‘KIERUJ¥CY’ ‘ANALITYK’ ‘PROGRAMISTA’; ograniczenie ma nosiæ 
---																		nazwê PRZYDZIALY_ROLA_CHK
+-- Nazwa		Typ					Rozmiar		WÅ‚asnoÅ›ci
+-- ID_PROJEKTU		Liczba caÅ‚kowita			4		Niepusty klucz obcy o nazwie PRZYDZIALY_FK_01 do kolumny ID_PROJEKTU w relacji PROJEKTY
+-- NR_PRACOWNIKA	Liczba caÅ‚kowita			6		Niepusty klucz obcy o nazwie PRZYDZIALY_FK_02 do kolumny ID_PRAC w relacji PRACOWNICY
+-- OD			Data							DomyÅ›lnie data systemowa
+-- DO			Data							PÃ³Åºniejsza niÅ¼ OD, ograniczenie ma nosiÄ‡ nazwÄ™ PRZYDZIALY_DATY_CHK
+-- STAWKA		Liczba					7,2		WiÄ™ksza od 0, ograniczenie ma nosiÄ‡ nazwÄ™ PRZYDZIALY_STAWKA_CHK
+-- ROLA			ÅaÅ„cuch znakÃ³w o zmiennej dÅ‚ugoÅ›ci	20		Dopuszczalne wartoÅ›ci to: â€˜KIERUJÄ„CYâ€™ â€˜ANALITYKâ€™ â€˜PROGRAMISTAâ€™; ograniczenie ma nosiÄ‡ 
+--																		nazwÄ™ PRZYDZIALY_ROLA_CHK
 --																					
--- Kluczem podstawowym relacji PRZYDZIALY jest para atrybutów (ID_PROJEKTU, NR_PRACOWNIKA), nazwa klucza to PRZYDZIALY_PK.
+-- Kluczem podstawowym relacji PRZYDZIALY jest para atrybutÃ³w (ID_PROJEKTU, NR_PRACOWNIKA), nazwa klucza to PRZYDZIALY_PK.
 -- 
 
 
-	CREATE TABLE projekty (id_projektu NUMBER(4) CONSTRAINT projekty_pk PRIMARY KEY
-                          ,opis_projektu VARCHAR2(20) NOT NULL CONSTRAINT projekty_uk UNIQUE
-                          ,data_rozpoczecia DATE DEFAULT SYSDATE
-                          ,data_zakonczenia DATE 
-						  ,fundusz NUMBER(7,2) CONSTRAINT projekty_fundusz_chk CHECK (fundusz >= 0) 
-						  ,CONSTRAINT projekty_daty_chk CHECK (data_zakonczenia > data_rozpoczecia));
+	CREATE TABLE projekty 
+	(id_projektu NUMBER(4) CONSTRAINT projekty_pk PRIMARY KEY
+        ,opis_projektu VARCHAR2(20) NOT NULL CONSTRAINT projekty_uk UNIQUE
+        ,data_rozpoczecia DATE DEFAULT SYSDATE
+        ,data_zakonczenia DATE 
+	,fundusz NUMBER(7,2) CONSTRAINT projekty_fundusz_chk CHECK (fundusz >= 0) 
+	,CONSTRAINT projekty_daty_chk CHECK (data_zakonczenia > data_rozpoczecia));
 
 
-	CREATE TABLE przydzialy (id_projektu NUMBER (4) NOT NULL CONSTRAINT przydzialy_fk_01 REFERENCES PROJEKTY(id_projektu)
-                            ,nr_pracownika NUMBER(6) NOT NULL CONSTRAINT przydzialy_fk_02 REFERENCES PRACOWNICY(id_prac)
-                            ,od DATE DEFAULT SYSDATE
-                            ,DO DATE, stawka NUMBER (7,2) CONSTRAINT przydzialy_stawka_chk CHECK (stawka > 0)
-							,rola VARCHAR2(20) CONSTRAINT przydzialy_rola_chk CHECK (rola IN ('KIERUJ¥CY', 'ANALITYK', 'PROGRAMISTA'))
-							,CONSTRAINT przydzialy_daty_chk CHECK (DO > od), CONSTRAINT przydzialy_pk PRIMARY KEY (id_projektu, nr_pracownika));
+	CREATE TABLE przydzialy 
+	(id_projektu NUMBER (4) NOT NULL CONSTRAINT przydzialy_fk_01 REFERENCES PROJEKTY(id_projektu)
+        ,nr_pracownika NUMBER(6) NOT NULL CONSTRAINT przydzialy_fk_02 REFERENCES PRACOWNICY(id_prac)
+        ,od DATE DEFAULT SYSDATE
+        ,DO DATE, stawka NUMBER (7,2) CONSTRAINT przydzialy_stawka_chk CHECK (stawka > 0)
+	,rola VARCHAR2(20) CONSTRAINT przydzialy_rola_chk CHECK (rola IN ('KIERUJÄ„CY', 'ANALITYK', 'PROGRAMISTA'))
+	,CONSTRAINT przydzialy_daty_chk CHECK (DO > od), CONSTRAINT przydzialy_pk PRIMARY KEY (id_projektu, nr_pracownika));
 --------------------------------------------------------
--- 2. Dodaj do relacji PRZYDZIALY atrybut GODZINY, bêd¹cy liczb¹ ca³kowit¹ o
---	  maksymalnej wartoœci równej 9999.
+-- 2. Dodaj do relacji PRZYDZIALY atrybut GODZINY, bÄ™dÄ…cy liczbÄ… caÅ‚kowitÄ… o
+--    maksymalnej wartoÅ›ci rÃ³wnej 9999.
 
 	ALTER TABLE przydzialy ADD godziny NUMBER(4); 
 
 --------------------------------------------------------
--- 3. Dodaj do utworzonych przez siebie relacji komentarze i wyœwietl te komentarze.
+-- 3. Dodaj do utworzonych przez siebie relacji komentarze i wyÅ›wietl te komentarze.
 
-	COMMENT ON TABLE projekty IS 'Lista projektów prowadzonych przez pracowników';
-	COMMENT ON TABLE przydzialy IS 'Informacje o przydziale poszczególnych pracowników do projektów';
+	COMMENT ON TABLE projekty IS 'Lista projektÃ³w prowadzonych przez pracownikÃ³w';
+	COMMENT ON TABLE przydzialy IS 'Informacje o przydziale poszczegÃ³lnych pracownikÃ³w do projektÃ³w';
 
 --------------------------------------------------------					 
--- 4. Wyœwietl informacje o ograniczeniach za³o¿onych na relacji PRZYDZIALY
+-- 4. WyÅ›wietl informacje o ograniczeniach zaÅ‚oÅ¼onych na relacji PRZYDZIALY
 
 	SELECT	constraint_name
-			,constraint_type
-			,search_condition
+		,constraint_type
+		,search_condition
 	FROM	user_constraints
 	WHERE	table_name = 'PRZYDZIALY';
 
 --------------------------------------------------------
--- 5. Wy³¹cz tymczasowo sprawdzanie unikalnoœci opisów projektów.
+-- 5. WyÅ‚Ä…cz tymczasowo sprawdzanie unikalnoÅ›ci opisÃ³w projektÃ³w.
 
 	ALTER TABLE projekty DISABLE CONSTRAINT projekty_uk; 
   
 --------------------------------------------------------
--- 6. Zwiêksz maksymalny rozmiar atrybutu OPIS_PROJEKTU do 30 znaków.
+-- 6. ZwiÄ™ksz maksymalny rozmiar atrybutu OPIS_PROJEKTU do 30 znakÃ³w.
 
 	ALTER TABLE projekty MODIFY opis_projektu VARCHAR2(30);
 
 --------------------------------------------------------
--- 7. Utwórz relacjê PRACOWNICY_ZESPOLY zawieraj¹c¹ poni¿sze dane (roczna p³aca to
---	  dwunastokrotnoœæ p³acy podstawowej plus p³aca dodatkowa). Pos³u¿ siê mechanizmem
---	  tworzenia relacji w oparciu o zapytanie.
+-- 7. UtwÃ³rz relacjÄ™ PRACOWNICY_ZESPOLY zawierajÄ…cÄ… poniÅ¼sze dane (roczna pÅ‚aca to
+--    dwunastokrotnoÅ›Ä‡ pÅ‚acy podstawowej plus pÅ‚aca dodatkowa). PosÅ‚uÅ¼ siÄ™ mechanizmem
+--    tworzenia relacji w oparciu o zapytanie.
 
 	CREATE TABLE pracownicy_zespoly (nazwisko
-									,posada
-									,roczna_placa
-									,zespol
-									,adres_pracy) AS
+					,posada
+					,roczna_placa
+					,zespol
+					,adres_pracy) AS
 	SELECT	p.nazwisko
-			,p.etat
-			,(p.placa_pod * 12) + NVL(p.placa_dod, 0) 
-			,z.nazwa AS zespol
-			,z.adres AS adres_pracy
+		,p.etat
+		,(p.placa_pod * 12) + NVL(p.placa_dod, 0) 
+		,z.nazwa AS zespol
+		,z.adres AS adres_pracy
 	FROM	pracownicy p
 	JOIN	zespoly z ON (p.id_zesp = z.id_zesp);
 	
 --------------------------------------------------------
---8. Aktywuj wy³¹czone w punkcie 5. ograniczenie.
+--8. Aktywuj wyÅ‚Ä…czone w punkcie 5. ograniczenie.
 	
 	ALTER TABLE projekty ENABLE CONSTRAINT projekty_uk;
 
