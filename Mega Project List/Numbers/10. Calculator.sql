dfd28b6f1f7ebb663ce9db5ad2aa2c0b97d8e3ca
MarcinMisiorny <30782851+MarcinMisiorny@users.noreply.github.com>
2017-08-29 18:52:36 +0200

    Create 10. Calculator.sql v1.0

---
+++ Mega Project List/Numbers/10. Calculator.sql
@@ -0,0 +1,61 @@
+/*
+* 
+* Case: Calculator
+* Description: A simple calculator to do basic operators. Make it a scientific calculator for added complexity.
+* 
+* My comment:
+* It's hard to do good calculator without graphical interface, so I made simple calculator as function. IMHO there is no need to do this 
+* more complexity.
+* 
+*/
+ 
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+/* My solution: */ 
+
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE FUNCTION fn_calc
+(n_first_number NUMBER
+,v_operation VARCHAR2
+,n_second_number NUMBER)
+RETURN NUMBER
+IS
+	v_result NUMBER;
+BEGIN
+	IF v_operation = '+' THEN
+		v_result := n_first_number + n_second_number;
+	ELSIF v_operation = '-'  THEN
+		v_result := n_first_number - n_second_number;
+	ELSIF v_operation = '*'  THEN
+		v_result := n_first_number * n_second_number;
+	ELSIF v_operation = '/'  THEN
+		v_result := n_first_number / n_second_number;
+	ELSIF v_operation = 'MOD'  THEN
+		v_result := MOD(n_first_number, n_second_number);
+	ELSIF v_operation = 'POWER'  THEN
+		v_result := POWER(n_first_number, n_second_number);
+	ELSIF v_operation = 'LOG'  THEN
+		v_result := LOG(n_first_number, n_second_number);
+	END IF;
+RETURN v_result;
+END fn_calc;
+/
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+/* Test: */
+SELECT	fn_calc(2,'+',2)
+FROM	dual;
+
+/* Script result: */
+4
+
+---
+/* Test: */
+SELECT	fn_calc(351,'POWER',7)
+FROM	dual;
+
+/* Script result: */
+656371650784449951
+
