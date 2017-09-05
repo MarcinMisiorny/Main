561b535956d2b4a43201fbe32d7fd951f68ede1b
Marcin Misiorny <marcin.misiorny@gmail.com>
2017-09-05 21:43:52 +0200

    Update 5. Next Prime Number.sql

--- Mega Project List/Numbers/5. Next Prime Number.sql
+++ Mega Project List/Numbers/5. Next Prime Number.sql
@@ -108,9 +108,9 @@ BEGIN
 
 	DBMS_OUTPUT.PUT_LINE(v_msg || v_result);
 EXCEPTION
-    WHEN ex_negative_number THEN
-        RAISE_APPLICATION_ERROR(-20001,
-        'Parameter ' || v_error_msg || ' cannot be less than 1. Value given by User: ' || v_error_parameter_msg);
+	WHEN ex_negative_number THEN
+		RAISE_APPLICATION_ERROR(-20001,
+		'Parameter ' || v_error_msg || ' cannot be less than 1. Value given by User: ' || v_error_parameter_msg);
 	WHEN OTHERS THEN
 		DBMS_OUTPUT.PUT_LINE(DBMS_UTILITY.FORMAT_ERROR_STACK);
 END pr_prime_list;
