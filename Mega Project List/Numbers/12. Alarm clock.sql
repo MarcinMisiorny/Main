774bbc67fff44ebc621d39b7e3911f2c62d2897f
MarcinMisiorny <30782851+MarcinMisiorny@users.noreply.github.com>
2017-10-05 18:48:27 +0200

    Create 12. Alarm clock.sql

---
+++ Mega Project List/Numbers/12. Alarm clock.sql
@@ -0,0 +1,14 @@
+/*
+* 
+* Case: Alarm clock
+* Description: A simple clock where it plays a sound after X number of minutes/seconds or at a particular time.
+* 
+* My comment:
+* I have some unexpected issues on my virtual machine - it does not play sounds, so I couldn't do this right way. 
+* Second, this case is probably impossible to do in way that I wanted to do. I wanted to create job, which will at given
+* time wrote something in output, but every job has unique session id. I wanted to wrote on output in my session, but job
+* exeutes code in different session id, so it's impossible to make my way. Maybe some day I'll figure out other method.
+* 
+*/
+ 
+
