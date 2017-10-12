300fc0349d0d8e88980c61ded8fdb5536a112bd8
MarcinMisiorny <30782851+MarcinMisiorny@users.noreply.github.com>
2017-10-12 18:03:51 +0200

    Dodano fn_waliduj_dowod_osobisty

--- Narzędzia/walidatory.sql
+++ Narzędzia/walidatory.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE PACKAGE walidatory
 IS
     FUNCTION fn_waliduj_pesel (p_pesel IN VARCHAR2) RETURN BOOLEAN;
     FUNCTION fn_waliduj_iban (p_numer_iban IN VARCHAR2) RETURN BOOLEAN;
+    FUNCTION fn_waliduj_dowod_osobisty (p_numer_dowodu IN VARCHAR2) RETURN BOOLEAN;
 END walidatory;
 /
 
@@ -37,7 +38,48 @@ IS
     EXCEPTION 
         WHEN OTHERS THEN RETURN FALSE;
     END fn_waliduj_pesel;
-
+	
+	FUNCTION fn_waliduj_dowod_osobisty
+    (p_numer_dowodu IN VARCHAR2)
+    RETURN BOOLEAN
+    IS
+       
+        n_liczba_kontrolna NUMBER;
+        v_litery VARCHAR2(3);
+        n_liczby NUMBER;
+        n_suma_czynnikow_litery NUMBER;
+        n_suma_czynnikow_liczby NUMBER;
+        b_wynik BOOLEAN;
+       
+        TYPE t_tab IS VARRAY(5) OF INTEGER;
+        t_wagi t_tab := t_tab(7, 3, 1, 7, 3);
+    BEGIN
+       
+        n_suma_czynnikow_litery := 0;
+        n_suma_czynnikow_liczby := 0;
+        v_litery := SUBSTR(p_numer_dowodu, 1, 3);
+        n_liczby := SUBSTR(p_numer_dowodu, 5, LENGTH(p_numer_dowodu));
+        n_liczba_kontrolna := SUBSTR(p_numer_dowodu, 4, 1);
+       
+        FOR i IN 1 .. LENGTH(v_litery) LOOP
+            n_suma_czynnikow_litery := n_suma_czynnikow_litery + ((ASCII(SUBSTR(v_litery, i, 1)) - 55) * t_wagi(i));
+        END LOOP;
+       
+        FOR i IN 1 .. LENGTH(n_liczby) LOOP
+            n_suma_czynnikow_liczby := n_suma_czynnikow_liczby + (SUBSTR(n_liczby, i, 1) * t_wagi(i));
+        END LOOP;
+       
+        IF MOD(n_suma_czynnikow_litery + n_suma_czynnikow_liczby, 10) = n_liczba_kontrolna THEN
+            b_wynik := TRUE;
+        ELSE
+            b_wynik := FALSE;
+        END IF;
+       
+    RETURN b_wynik;
+    EXCEPTION
+       WHEN OTHERS THEN
+            RETURN FALSE;
+    END fn_waliduj_dowod_osobisty;
 
     FUNCTION fn_waliduj_iban
     (p_numer_iban IN VARCHAR2)
