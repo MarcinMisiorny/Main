e7a7664d363ec812cf3e64684fb64b70d334d8c9
MarcinMisiorny <30782851+MarcinMisiorny@users.noreply.github.com>
2017-10-23 22:27:05 +0200

    Create 17. Complex Number Algebra.sql v1.0

---
+++ Mega Project List/Numbers/17. Complex Number Algebra.sql
@@ -0,0 +1,25 @@
+/*
+* 
+* Case: Complex Number Algebra
+* Description: Show addition, multiplication, negation, and inversion of complex numbers in separate functions. (Subtraction and division 
+* operations can be made with pairs of these operations.) Print the results for each operation tested.
+* 
+* My comment: 
+*  
+* 
+*/
+ 
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+/* My solution: */ 
+
+SET SERVEROUTPUT ON;
+
+-- in progress
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+/* Test: */
+
+
+/* Script result: */
