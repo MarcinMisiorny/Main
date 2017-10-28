fdd190c3db65a1b5c3c8dd06e798db595f699be4
MarcinMisiorny <30782851+MarcinMisiorny@users.noreply.github.com>
2017-10-28 22:30:21 +0200

    Create Limit calculator.sql v1.0

---
+++ Mega Project List/Numbers/21. Limit calculator.sql
@@ -0,0 +1,28 @@
+/*
+* 
+* Case: Limit calculator
+* Description: Ask the user to enter f(x) and the limit value, then return the value of the limit statement. 
+* Optional: Make the calculator capable of supporting infinite limits.
+* 
+* My comment: 
+* 
+* 
+*/
+ 
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+/* My solution: */ 
+
+SET SERVEROUTPUT ON;
+
+--in progress
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+/* Test: */
+
+
+/* Script result: */
+
+---
+
