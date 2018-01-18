7083e4e414cfae07ab0aa05fb86410dea50815c5
Marcin Misiorny <marcin.misiorny@gmail.com>
2018-01-18 21:55:45 +0100

    Update zamiana polskich znaków.sql

--- Narzędzia/zamiana polskich znaków.sql
+++ Narzędzia/zamiana polskich znaków.sql
@@ -4,9 +4,9 @@ CREATE OR REPLACE FUNCTION fn_usun_polskie_znaki
 (p_tekst VARCHAR2)
 RETURN VARCHAR2
 IS
-	v_tekst_bez_polskich_znakow VARCHAR2(4000);
+    v_tekst_bez_polskich_znakow VARCHAR2(4000);
 BEGIN
-	v_tekst_bez_polskich_znakow := TRANSLATE(p_tekst, 'Ą, Ć, Ę, Ł, Ń, Ó, Ś, Ź, Ż, ą, ć, ę, ł, ń, ó, ś, ź, ż', 'A, C, E, L, N, O, S, Z, Z, a, c, e, l, n, o, s, z, z');
+    v_tekst_bez_polskich_znakow := TRANSLATE(p_tekst, 'Ą, Ć, Ę, Ł, Ń, Ó, Ś, Ź, Ż, ą, ć, ę, ł, ń, ó, ś, ź, ż', 'A, C, E, L, N, O, S, Z, Z, a, c, e, l, n, o, s, z, z');
 	
 RETURN v_tekst_bez_polskich_znakow;
 END fn_usun_polskie_znaki;
